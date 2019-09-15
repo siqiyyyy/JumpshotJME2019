fdf50f68a985c8891ad7172518ac85dddec26452
Siqi <yuansiqi0114@hotmail.com>
2019-09-14 22:28:02 -0500

    remove npv bining, add plot for puppi; improved the plots

--- plot_resolution.py
+++ plot_resolution.py
@@ -47,7 +47,7 @@ def plot_hists(hist_list, title="", legend_title_list=None, x_title="", y_title=
 		latex.Draw("same")
 	if legend_title_list==None:
 		legend_title_list = [i.GetTitle() for i in hist_list]
-	legend=ROOT.TLegend(0.60,0.80,0.95,.95)
+	legend=ROOT.TLegend(0.60,0.80,0.95,.91)
 	for ihist,hist in enumerate(hist_list):
 		legend.AddEntry(hist,legend_title_list[ihist],"lp")
 	legend.Draw("same")
@@ -58,104 +58,109 @@ def plot_hists(hist_list, title="", legend_title_list=None, x_title="", y_title=
 
 
 
-def get_met_params(rg): #given a phase space range, return response, sigma u_pll, sigma uprp, and their errors																							 params={} #this dict is to be returned
-	#get the responses
-	#mean of upll devided by the mean of qt
-	#do the same thing for both pf and puppi
+def get_met_params(rg): #given a phase spacific range, return response, resolution, and their errors
+	params={} # we will fill this dict and return it
+
 	print "getting params in range: " + rg
 	print "number of events in range: " + str(t_in.GetEntries(rg))
-	_shape=np.arange(-200,200,5)
-	shape_qt = ROOT.TH1F("shape_qt","shape_qt",len(_shape)-1,array('d',_shape))
-	shape_pf = ROOT.TH1F("shape_pf","shape_pf",len(_shape)-1,array('d',_shape)) #this can be used to fill both parallel and perpendicular recoil (overwritten)
-	shape_puppi = ROOT.TH1F("shape_puppi","shape_puppi",len(_shape)-1,array('d',_shape)) #this can be used to fill both parallel and perpendicular recoil (overwritten)
-	t_in.Draw("qt>>shape_qt", rg)
-	t_in.Draw("u_pll_pf>>shape_pf", rg)
-	params={}
-	if shape_qt.GetMean()==0 or shape_pf.GetMean()==0: return None
-	params["response_pf"]=abs(shape_pf.GetMean()/shape_qt.GetMean())
-	params["response_pf_error"]=sqrt((shape_pf.GetMeanError()/shape_pf.GetMean())**2+(shape_qt.GetMeanError()/shape_qt.GetMean())**2) * params["response_pf"]
-	t_in.Draw("u_pll_puppi>>shape_puppi",rg)
-	if shape_puppi.GetMean()==0: return None
-	params["response_puppi"]=abs(shape_puppi.GetMean()/shape_qt.GetMean())
-	params["response_puppi_error"]=sqrt((shape_puppi.GetMeanError()/shape_puppi.GetMean())**2+(shape_qt.GetMeanError()/shape_qt.GetMean())**2) * params["response_puppi"]
-	plot_hists([shape_qt,shape_pf,shape_puppi], legend_title_list=["q_{T}", "u_{||}(PF)","u_{||}(PUPPI)"], x_title="p_{T}(GeV)", y_title="Events/5GeV", plot_name="fit/qt_upll"+rg,text_description=rg)
-	t_in.Draw("u_pll_pf>>shape_pf",rg)
-	if shape_pf.GetStdDev()<=0: return None
-	params["sigma_pll_pf"]=shape_pf.GetStdDev()
-	params["sigma_pll_pf_error"]=shape_pf.GetStdDevError()
-	params["resolution_pll_pf"]=params["sigma_pll_pf"]/params["response_pf"]
-	params["resolution_pll_pf_error"]=sqrt((params["sigma_pll_pf_error"]/params["sigma_pll_pf"])**2+(params["response_pf_error"]/params["response_pf"])**2)*params["resolution_pll_pf"]
-	t_in.Draw("u_pll_puppi>>shape_puppi",rg)
-	if shape_puppi.GetStdDev()<=0: return None
-	params["sigma_pll_puppi"]=shape_puppi.GetStdDev()
-	params["sigma_pll_puppi_error"]=shape_puppi.GetStdDevError()
-	params["resolution_pll_puppi"]=params["sigma_pll_puppi"]/params["response_puppi"] 
-	params["resolution_pll_puppi_error"]=sqrt((params["sigma_pll_puppi_error"]/params["sigma_pll_puppi"])**2+(params["response_puppi_error"]/params["response_puppi"])**2)*params["resolution_pll_puppi"]
-	t_in.Draw("u_prp_pf>>shape_pf",rg)
-	if shape_pf.GetStdDev()<=0: return None
-	params["sigma_prp_pf"]=shape_pf.GetStdDev()
-	params["sigma_prp_pf_error"]=shape_pf.GetStdDevError()
-	params["resolution_prp_pf"]=params["sigma_prp_pf"]/params["response_pf"]
-	params["resolution_prp_pf_error"]=sqrt((params["sigma_prp_pf_error"]/params["sigma_prp_pf"])**2+(params["response_pf_error"]/params["response_pf"])**2)*params["resolution_prp_pf"]
-	t_in.Draw("u_prp_puppi>>shape_puppi",rg)
-	if shape_puppi.GetStdDev()<=0: return None
-	params["sigma_prp_puppi"]=shape_puppi.GetStdDev()
-	params["sigma_prp_puppi_error"]=shape_puppi.GetStdDevError()
-	params["resolution_prp_puppi"]=params["sigma_prp_puppi"]/params["response_puppi"]
-	params["resolution_prp_puppi_error"]=sqrt((params["sigma_prp_puppi_error"]/params["sigma_prp_puppi"])**2+(params["response_puppi_error"]/params["response_puppi"])**2)*params["resolution_prp_puppi"]
-	plot_hists([shape_pf,shape_puppi], legend_title_list=["PF met","PUPPI MET"], x_title="u_{#perp}(GeV)", y_title="Events/5GeV", plot_name="fit/uprp"+rg,text_description=rg)
+	if t_in.GetEntries(rg) < 5:
+		print "Warning: number of entries too smale in this region, abort"
+		return None
+	
+	# Histograms to be filled
+	_pt_shape=np.arange(-200,200,5)
+	_ratio_shape=np.arange(-3.,5.,0.02)
+	shape_pf_response = ROOT.TH1F("shape_pf_response","shape_pf_response",len(_ratio_shape)-1,array('d',_ratio_shape))
+	shape_puppi_response = ROOT.TH1F("shape_puppi_response","shape_puppi_response",len(_ratio_shape)-1,array('d',_ratio_shape))
+	shape_pf_pll = ROOT.TH1F("shape_pf_pll","shape_pf_pll",len(_pt_shape)-1,array('d',_pt_shape)) 
+	shape_puppi_pll = ROOT.TH1F("shape_puppi_pll","shape_puppi_pll",len(_pt_shape)-1,array('d',_pt_shape))
+	shape_pf_prp = ROOT.TH1F("shape_pf_prp","shape_pf_prp",len(_pt_shape)-1,array('d',_pt_shape)) 
+	shape_puppi_prp = ROOT.TH1F("shape_puppi_prp","shape_puppi_prp",len(_pt_shape)-1,array('d',_pt_shape))
+
+	# Fill histograms
+	t_in.Draw("-u_pll_pf/qt>>shape_pf_response",rg)
+	t_in.Draw("-u_pll_puppi/qt>>shape_puppi_response",rg)
+	t_in.Draw("u_pll_pf+qt>>shape_pf_pll",rg)
+	t_in.Draw("u_pll_puppi+qt>>shape_puppi_pll",rg)
+	t_in.Draw("u_prp_pf+qt>>shape_pf_prp",rg)
+	t_in.Draw("u_prp_puppi+qt>>shape_puppi_prp",rg)
+
+
+	#Obtain the parameters
+	#For simplicity we are taking directly the mean and RMS of histograms here
+	#But to do it more accurately you would want to do a fit
+	params["response_pf"] = shape_pf_response.GetMean()
+	params["response_pf_error"] = shape_pf_response.GetMeanError()
+	params["response_puppi"] = shape_puppi_response.GetMean()
+	params["response_puppi_error"] = shape_puppi_response.GetMeanError()
+	params["resolution_pll_pf"] = shape_pf_pll.GetRMS() / params["response_pf"]
+	params["resolution_pll_pf_error"] = sqrt( pow(shape_pf_pll.GetRMSError(),2) + pow( params["resolution_pll_pf"]*params["response_pf_error"]/params["response_pf"],2))
+	params["resolution_pll_puppi"] = shape_puppi_pll.GetRMS() / params["response_puppi"]
+	params["resolution_pll_puppi_error"] = sqrt( pow(shape_puppi_pll.GetRMSError(),2) + pow( params["resolution_pll_puppi"]*params["response_puppi_error"]/params["response_puppi"],2))
+	params["resolution_prp_pf"] = shape_pf_prp.GetRMS() / params["response_pf"]
+	params["resolution_prp_pf_error"] = sqrt( pow(shape_pf_prp.GetRMSError(),2) + pow( params["resolution_prp_pf"]*params["response_pf_error"]/params["response_pf"],2))
+	params["resolution_prp_puppi"] = shape_puppi_prp.GetRMS() / params["response_puppi"]
+	params["resolution_prp_puppi_error"] = sqrt( pow(shape_puppi_prp.GetRMSError(),2) + pow( params["resolution_prp_puppi"]*params["response_puppi_error"]/params["response_puppi"],2))
+
+	plot_hists([shape_pf_response,shape_puppi_response], legend_title_list=["PF met","PUPPI MET"], x_title="-u_{||}/q_{T}", y_title="Events", plot_name="fit/response_"+rg,text_description=rg)
+	plot_hists([shape_pf_pll,shape_puppi_pll], legend_title_list=["PF met","PUPPI MET"], x_title="u_{||}+q_{T}", y_title="Events", plot_name="fit/pll_"+rg,text_description=rg)
+	plot_hists([shape_pf_prp,shape_puppi_prp], legend_title_list=["PF met","PUPPI MET"], x_title="u_{#perp}", y_title="Events", plot_name="fit/prp_"+rg,text_description=rg)
 	return params
 
-# Create one TMultigraph for each of the following measurement
-mg_metResponse_pf = ROOT.TMultiGraph()
-mg_metResolution_pf = ROOT.TMultiGraph()
-#Loop through npv bins
-for inpv in range(len(_npv)-1):
-	sel_npv="npv>"+str(_npv[inpv])+"&&npv<"+str(_npv[inpv+1])
-	leg_npv=str(_npv[inpv])+" < npv < " + str(_npv[inpv+1])
-	li_metResponse_pf = []
-	li_metResponse_pf_error = []
-	li_metResolution_pf = []
-	li_metResolution_pf_error = []
-	li_pt_center = []
-	li_pt_halfwidth = []
-	#loop through Z pt bins
-	for ipt in range(len(_pt)-1):
-		ptlow, pthigh = _pt[ipt], _pt[ipt+1]
-		sel_qt="qt>"+str(_pt[ipt])+"&&qt<"+str(_pt[ipt+1])
-		li_pt_center.append(0.5*ptlow + 0.5*pthigh)
-		li_pt_halfwidth.append(pthigh-ptlow)
-		params=get_met_params(sel_npv+"&&"+sel_qt)
-		if params:
-			#print(sel_npv+"&&"+sel_qt)
-			#print(params)
-			li_metResponse_pf.append(params["response_pf"])
-			li_metResponse_pf_error.append(params["response_pf_error"])
-			li_metResolution_pf.append(params["resolution_pll_pf"])
-			li_metResolution_pf_error.append(params["resolution_pll_pf_error"])
-		else: #in case nothing returne due to limited stats
-			li_metResponse_pf.append(0)
-			li_metResponse_pf_error.append(1)
-			li_metResolution_pf.append(0)
-			li_metResolution_pf_error.append(1)
-	# transfer list into TGraph:
-	npoints=len(li_pt_center)
-	g_metResponse_pf = ROOT.TGraphErrors(npoints, array('d',li_pt_center),  array('d',li_metResponse_pf),array('d',li_pt_halfwidth), array('d',li_metResponse_pf_error))
-	g_metResolution_pf = ROOT.TGraphErrors(npoints, array('d',li_pt_center), array('d',li_metResponse_pf), array('d',li_pt_halfwidth), array('d',li_metResponse_pf_error))
-	g_metResponse_pf.SetTitle(leg_npv)
-	g_metResolution_pf.SetTitle(leg_npv)
-	mg_metResponse_pf.Add(g_metResponse_pf)
-	mg_metResolution_pf.Add(g_metResolution_pf)
+# Create histograms to store the parameters
+h_response_pf = ROOT.TH1F("h_response_pf","pf met scale",len(_pt)-1,array('d',_pt))
+h_response_puppi = ROOT.TH1F("h_response_puppi","puppi met scale",len(_pt)-1,array('d',_pt))
+h_resolution_pll_pf = ROOT.TH1F("h_resolution_pll_pf","pf met resolution parallel",len(_pt)-1,array('d',_pt))
+h_resolution_pll_puppi = ROOT.TH1F("h_resolution_pll_puppi","puppi met resolution parallel",len(_pt)-1,array('d',_pt))
+#loop through Z pt bins
+for ipt in range(len(_pt)-1):
+	ptlow, pthigh = _pt[ipt], _pt[ipt+1]
+	sel_qt="qt>"+str(_pt[ipt])+"&&qt<"+str(_pt[ipt+1])
+	params=get_met_params(sel_qt)
+	if params:
+		#print(sel_qt)
+		#print(params)
+		h_response_pf.SetBinContent(ipt+1,params["response_pf"])
+		h_response_pf.SetBinError(ipt+1,params["response_pf_error"])
+		h_response_puppi.SetBinContent(ipt+1,params["response_puppi"])
+		h_response_puppi.SetBinError(ipt+1,params["response_puppi_error"])
+		h_resolution_pll_pf.SetBinContent(ipt+1,params["resolution_pll_pf"])
+		h_resolution_pll_pf.SetBinError(ipt+1,params["resolution_pll_pf_error"])
+		h_resolution_pll_puppi.SetBinContent(ipt+1,params["resolution_pll_puppi"])
+		h_resolution_pll_puppi.SetBinError(ipt+1,params["resolution_pll_puppi_error"])
+	else:
+		#in case return value is none, possibly no stat in this region
+		#you can do something about it like setting big error bars
+		continue
 
 
+ROOT.gStyle.SetPalette(ROOT.kDarkRainBow)
 
 # Make plot for the MET response
 c1 = ROOT.TCanvas("c1","c1",800,800)
-mg_metResponse_pf.Draw("AP")
-c1.BuildLegend()
+h_response_pf.SetLineColor(ROOT.kRed)
+h_response_pf.SetAxisRange(0,1.5,"Y")
+h_response_pf.GetXaxis().SetTitle("q_{T}/GeV")
+h_response_pf.GetYaxis().SetTitle("u_{||}/q_{T}")
+h_response_pf.Draw()
+h_response_puppi.SetLineColor(ROOT.kBlue)
+h_response_puppi.Draw("same")
+leg1=ROOT.TLegend(0.55,0.1,0.9,0.3)
+leg1.AddEntry(h_response_pf,"PF+CHS MET")
+leg1.AddEntry(h_response_puppi,"PUPPI MET")
+leg1.Draw("same")
 c1.Print("result/met_response.png")
 # Make plot for the MET resolution
 c2 = ROOT.TCanvas("c2","c2",800,800)
-mg_metResolution_pf.Draw("AP")
-c2.BuildLegend()
-c2.Print("result/met_resolution.png")
+h_resolution_pll_pf.SetLineColor(ROOT.kRed)
+h_resolution_pll_pf.SetAxisRange(0,50,"Y")
+h_resolution_pll_pf.GetXaxis().SetTitle("q_{T}/GeV")
+h_resolution_pll_pf.GetYaxis().SetTitle("RMS(u_{||}+q_{T})")
+h_resolution_pll_pf.Draw()
+h_resolution_pll_puppi.SetLineColor(ROOT.kBlue)
+h_resolution_pll_puppi.Draw("same")
+leg2=ROOT.TLegend(0.55,0.1,0.9,0.3)
+leg2.AddEntry(h_resolution_pll_pf,"PF+CHS MET")
+leg2.AddEntry(h_resolution_pll_puppi,"PUPPI MET")
+leg2.Draw("same")
+c2.Print("result/met_resolution_pll.png")
